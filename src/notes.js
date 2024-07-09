f95808f13d904ca9b1323f5aad65089807d96c2e
Aditya Kushwaha <kushwaha07adity@gmail.com>
2024-07-09 07:56:12 +0530

    first commit

---
+++ src/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+    {
+    key: 1,
+    title: "Delegation",
+    content:
+    "Q.How many programmers does it take to change"
+    },
+    {
+    key: 2,
+    title: "Loops",
+    content:
+    "Q.How to keep a programmer in the shower forever"
+    },
+    {
+    key: 3,
+    title: "Arrays",
+    content:
+    "Q. Why did the programmer quit his job? A. Bec"
+    }
+];
+
+export default notes
